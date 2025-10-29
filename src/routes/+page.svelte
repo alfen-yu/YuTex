a841057b738b5dcffa84545631e3ec4d845aa00d
Yousuf Uyghur <yousufuighur@yahoo.com>
2025-10-29 10:43:23 +0500

    setup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,10 +12,10 @@
 </script>
 
 <main class="container">
-  <h1>Welcome to Tauri + Svelte</h1>
+  <h1>A Latex Code Editor</h1>
 
   <div class="row">
-    <a href="https://vite.dev" target="_blank">
+    <!-- <a href="https://vite.dev" target="_blank">
       <img src="/vite.svg" class="logo vite" alt="Vite Logo" />
     </a>
     <a href="https://tauri.app" target="_blank">
@@ -23,7 +23,9 @@
     </a>
     <a href="https://svelte.dev" target="_blank">
       <img src="/svelte.svg" class="logo svelte-kit" alt="SvelteKit Logo" />
-    </a>
+    </a> -->
+
+    Hello World
   </div>
   <p>Click on the Tauri, Vite, and SvelteKit logos to learn more.</p>
 
